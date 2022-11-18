bb4503482cf7944b649c5168df7b77cbcf92bdd9
Sanjar <sanzharkadyarliev@gmail.com>
2022-11-18 20:04:14 +0600

    draw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 choice = ['', 'n', 'b']
+count = [0, 0]
 def check_answer(t):
     if t == "k":
         print("kamen")
@@ -18,8 +19,13 @@ def main():
         check_answer(c_turn)
         if turn == "k" and c_turn == "n" or turn == "b" and c_turn == "k" or turn == "n" and c_turn == "b":
             print("you win")
+            count[0] += 1
         elif c_turn == "k" and turn == "n" or c_turn == "b" and turn == "k" or c_turn == "n" and turn == "b":
             print("comp win")
-
+            count[1] += 1
+        print("score:", *count)
+        if turn == "k" and c_turn == "k" or turn == "b" and c_turn == "b" or turn == "n" and c_turn == "n":
+            print("draw")
+        
 main()
 
