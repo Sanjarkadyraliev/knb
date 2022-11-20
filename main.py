efcba730914e5321dba6f93b4b4d6c2cb938a21c
Sanjar <sanzharkadyarliev@gmail.com>
2022-11-20 23:03:45 +0600

    playagain not working

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import random
 choice = ['k', 'n', 'b']
 count = [0, 0]
 def check_answer(t):
-    if t == "k":
-        print("kamen")
-    elif t == "b":
-        print("bumaga")
-    elif t == "n":
-        print("nozhnicy")
+        if t == "k":
+            print("kamen")
+        elif t == "b":
+            print("bumaga")
+        elif t == "n":
+            print("nozhnicy")
+
 def main():
     while checkTotalScore():
         turn = input("Please enter your turn: ('k', 'n', 'b'): ").lower()
@@ -18,6 +19,13 @@ def main():
         c_turn = random.choice(choice)
         check_answer(c_turn)
         score(turn,c_turn)
+    while playAgain():
+        check_answer(turn)
+        main()
+        c_turn = random.choice(choice)
+        check_answer(c_turn)
+        score(turn, c_turn)
+
 
 def checkTotalScore():
     if count[0] == 3:
@@ -37,9 +45,17 @@ def score (turn, c_turn):
     if turn == "k" and c_turn == "k" or turn == "b" and c_turn == "b" or turn == "n" and c_turn == "n":
         print("draw")
 
-    if(count[0] > count[1] and count[0] == 3):
+    if count[0] > count[1] and count[0] == 3:
         print("You win the game")
-    if(count[1] > count[0] and count[1] == 3):
-        print("PC win the game")
+    if count[1] > count[0] and count[1] == 3:
+        print("comp win the game")
+def playAgain():
+    response = input("Do you wanna play again:(yes or no): ").upper()
+    if response =="Yes":
+        return True
+    elif response == "No":
+        return False
+
+
 main()
 
